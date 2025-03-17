18ad58f05b6fcd377d96f08d8806916d51c1b32f
MaxxBahr <schnee.bahr@googlemail.com>
2025-03-17 19:13:20 +0100

    added database and tokenization of keywords

--- my_compiler/lexer/mod.rs
+++ my_compiler/lexer/mod.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use rusqlite::{Connection};
 pub enum Tokens {
     Number,
     Identifier,
@@ -8,7 +9,24 @@ pub enum Tokens {
     OpenBrace,
     CloseBrace,
     BinaryOperator,
+    VariableName,
     Variable,
+    ReturnValue,
+}
+
+fn tokenize_keyword(token: &str) -> Option<Tokens>{
+    match token {
+        "Variable" => Some(Tokens::Variable),
+        "ReturnValue" => Some(Tokens::ReturnValue),
+        _ => None,
+    }
+}
+
+fn is_keyword(word: &str) -> Result<Tokens, rusqlite::Error> {
+    let conn = Connection::open("keywords.db")?;
+    let mut stmt = conn.prepare("SELECT token FROM words WHERE word =?1")?;
+    let token = stmt.query_row([word], |row| row.get(0))?;
+    tokenize_keyword(token).ok_or_else(|| rusqlite::Error::InvalidQuery)
 }
 
 pub struct Token {
@@ -17,18 +35,18 @@ pub struct Token {
 }
 
 impl Token {
-    pub fn new(value: String, token: String) -> Token {
+    pub fn new(value: String, token: Tokens) -> Token {
         Token { value, token }
     }
 }
 
 pub fn tokenizer(code: &str) -> HashMap<i32, Token> {
     let sep_code: Vec<&str> = split_string(code);
-    let mut result: HashMap<i32, Token>;
+    let mut result: HashMap<i32, Token> = HashMap::new();
 
     for word in sep_code {
         if let Ok(number) = word.parse::<i32>() {
-            result.insert(result.len(), Token::new(number, Tokens::Number));
+            result.insert(result.len(), Token::new(word, Tokens::Number));
         }
         if word == "("{
             result.insert(result.len(), Token::new(word, Tokens::OpenParen));
@@ -42,12 +60,14 @@ pub fn tokenizer(code: &str) -> HashMap<i32, Token> {
         if word == "}"{
             result.insert(result.len(), Token::new(word, Tokens::CloseBrace));
         }
-        // If there is a keyword like let we have to store the variables name
-        // Map Variable, operator, braces, parantheses, Identifier
+        // If there is a keyword like let we have to store the VariableNames name
+
+        // Map VariableName, operator, braces, parantheses, Identifier
     }
+    result
 }
 
 fn split_string(code: &str) -> Vec<&str> {
-    let sep_code: Vec<&str> = code.trim().split(' ').collect();
+    let sep_code: Vec<&str> = code.trim().split_whitespace().collect();
     sep_code
 }
