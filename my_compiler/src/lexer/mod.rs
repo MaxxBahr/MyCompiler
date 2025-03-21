14f5d911fa070add0436be87dbbe3d449a31a088
MaxxBahr <schnee.bahr@googlemail.com>
2025-03-21 16:24:54 +0100

    wip

--- my_compiler/src/lexer/mod.rs
+++ my_compiler/src/lexer/mod.rs
@@ -1,22 +1,29 @@
 use std::collections::{HashMap, HashSet};
 use std::path::Path;
-use rusqlite::{Connection};
+use std::process::exit;
+
+static HADERROR: bool = false;
+
 #[derive(Debug)]
 pub enum Tokens {
-    Number,
-    Identifier,
-    Equals,
-    OpenParen,
-    CloseParen,
-    OpenBrace,
-    CloseBrace,
-    BinaryOperator,
-    VariableName,
-    Variable,
-    ReturnValue,
-    VariableType,
-    FunctionName,
-    Operator(Operators),
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords.
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
 }
 #[derive(Debug)]
 enum Operators {
@@ -50,6 +57,9 @@ pub fn lexer(args: String){
 fn runFile(args: String){
     let code = std::io::read_to_string(Path::new(&args)).unwrap();
     run(code);
+    if HADERROR {
+        exit(0);
+    }
 }
 
 fn runPrompt(){
@@ -67,10 +77,27 @@ fn runPrompt(){
         }
         if input.is_empty(){break;}
         run(input.clone());
+        &HADERROR = &false;
+
+
     }
 }
 
 fn run(input: String){
     let mut scanner = Scanner::new(input);
-    let tokens:
+    let tokens: Vec<Token> = scanner.scanTokens();
+
+    for token in tokens{
+        println!("{:?}", token);
+    }
 }
+
+pub fn error(line: i32, message: String){
+    report(line, "".to_string(), message);
+}
+
+pub fn report(line: i32, here: String, message: String){
+    println!("[line: {line}] Error {here}: {message}");
+    &HADERROR = &true;
+}
+
